23d2d9cd8eb02e483e97434d58f6d21e6cf86d6f
hamaouic <hamaoui.ux@gmail.com>
2026-03-26 16:46:11 -0300

    Add "Looking to post a job?" text with underlined link below Answer the Call button

--- app/page.tsx
+++ app/page.tsx
@@ -86,6 +86,7 @@ export default function Home() {
               Answer the Call
             </Link>
           </div>
+          <p className="text-sm text-white text-center mt-6">Looking to post a job? <Link href="/listings/pricing" className="underline hover:text-lime-200 transition">Click here</Link></p>
         </div>
       </div>
 
